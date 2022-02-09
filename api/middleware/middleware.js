791e781e25e2844b4af59c01df4b2b9fbe281ef5
Brittany Petersen <britt.petersen100@gmail.com>
2022-02-09 13:27:39 -0700

    added post request with validateUser middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -23,7 +23,7 @@ async function validateUserId(req, res, next) {
   let result = await Users.getById(id);
   if(!result) {
     res.status(404).json({
-      message: 'user not found'
+      message: 'user id not found'
     });
   } else {
     req.user = result;
@@ -33,10 +33,24 @@ async function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  if (!req.body.name) {
+    res.status(400).json({
+      message: 'missing required name field'
+    })
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  if (!req.body.text) {
+    res.status(400).json({
+      message: 'missing required text field'
+    })
+  } else {
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
